17c469f6d85bacf8ff6eabc3559e4ae265aaa410
randyzwitch <rzwitch@gmail.com>
2018-12-14 08:54:25 -0500

    Add rsid keyword to GetReportSuites

--- R/GetReportSuites.R
+++ R/GetReportSuites.R
@@ -7,15 +7,24 @@
 #' @export
 #'
 #' @examples
-GetReportSuites <- function(globalCompanyId, as.data.frame=TRUE) {
+GetReportSuites <- function(globalCompanyId, rsid=NULL, as.data.frame=TRUE) {
 
   endpoint <- sprintf("https://analytics.adobe.io/api/%s",
                       globalCompanyId)
+  resource <- "/collections/suites"
 
-  r <- adobe_get(endpoint, "/collections/suites", AdobeRInternals$auth, globalCompanyId)
+  if(!is.null(rsid)){
+    resource <- paste(resource, "/", rsid, sep="")
+  }
+
+  r <- adobe_get(endpoint, resource, AdobeRInternals$auth, globalCompanyId)
 
   #Set S3 method for easier parsing later
-  class(r) <- "ReportSuites"
+  if(is.null(rsid)){
+    class(r) <- "ReportSuites"
+  } else {
+    class(r) <- "ReportSuite"
+  }
 
   #Return a data.frame or just an S3 object
   if(as.data.frame){
@@ -35,6 +44,24 @@ as.data.frame.ReportSuites <- function(x) {
 
 #' @export
 #' @keywords internal
-CompanyAccess <- function(x) {
+ReportSuites <- function(x) {
   UseMethod("ReportSuites", x)
+}
+
+#' @export
+#' @keywords internal
+as.data.frame.ReportSuite <- function(x) {
+
+  df <- as.data.frame(list(collectionItemType = x$response$collectionItemType,
+                           rsid = x$response$rsid)
+                      )
+
+  return(df)
+
+}
+
+#' @export
+#' @keywords internal
+ReportSuite <- function(x) {
+  UseMethod("ReportSuite", x)
 }
