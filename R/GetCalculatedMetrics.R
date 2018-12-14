d38b990e67e4168a6941c6e2ee023da6d06567fe
randyzwitch <rzwitch@gmail.com>
2018-12-13 23:02:38 -0500

    Add id argument to GetCalculatedMetrics
    Adding id returns a slightly different structure than without it, so different S3 methods need to be defined

--- R/GetCalculatedMetrics.R
+++ R/GetCalculatedMetrics.R
@@ -1,21 +1,30 @@
 #' GetCalculatedMetrics
 #'
 #' @param globalCompanyId
+#' @param id
 #' @param as.data.frame
 #'
 #' @return
 #' @export
 #'
 #' @examples
-GetCalculatedMetrics <- function(globalCompanyId, as.data.frame=TRUE) {
+GetCalculatedMetrics <- function(globalCompanyId, id=NULL, as.data.frame=TRUE) {
 
-  endpoint <- sprintf("https://analytics.adobe.io/api/%s",
-                      globalCompanyId)
+  endpoint <- sprintf("https://analytics.adobe.io/api/%s", globalCompanyId)
+  resource <- "/calculatedmetrics"
 
-  r <- adobe_get(endpoint, "/calculatedmetrics", AdobeRInternals$auth, globalCompanyId)
+  if(!is.null(id)){
+    resource <- paste(resource, "/", id, sep="")
+  }
+
+  r <- adobe_get(endpoint, resource, AdobeRInternals$auth, globalCompanyId)
 
   #Set S3 method for easier parsing later
-  class(r) <- "CalculatedMetrics"
+  if(is.null(id)){
+    class(r) <- "CalculatedMetrics"
+  } else {
+    class(r) <- "CalculatedMetric"
+  }
 
   #Return a data.frame or just an S3 object
   if(as.data.frame){
@@ -35,6 +44,36 @@ as.data.frame.CalculatedMetrics <- function(x) {
 
 #' @export
 #' @keywords internal
-CompanyAccess <- function(x) {
+CalcMetrics <- function(x) {
   UseMethod("CalculatedMetrics", x)
+}
+
+#' @export
+#' @keywords internal
+as.data.frame.CalculatedMetric <- function(x) {
+
+  if(is.null(x$response$description)){
+    x$response$description <- NA
+  }
+
+  df <- as.data.frame(list(id = x$response$id,
+                           name = x$response$name,
+                           description = x$response$description,
+                           rsid = x$response$rsid,
+                           polarity = x$response$polarity,
+                           precision = x$response$precision,
+                           type = x$response$type,
+                           owner.id = x$response$owner$id)
+                      )
+
+  #names(df) <- c("id", "name", "description", "rsid", "polarity", "precision", "type", "owner.id")
+
+  return(df)
+
+}
+
+#' @export
+#' @keywords internal
+CalcMetric <- function(x) {
+  UseMethod("CalculatedMetric", x)
 }
