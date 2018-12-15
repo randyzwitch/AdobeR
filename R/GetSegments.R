f9cd8d1b940e98263ed6abe12360e758857efe1b
randyzwitch <rzwitch@gmail.com>
2018-12-15 14:12:06 -0500

    Add id to GetSegments

--- R/GetSegments.R
+++ R/GetSegments.R
@@ -1,21 +1,32 @@
 #' GetSegments
 #'
 #' @param globalCompanyId
+#' @param id
 #' @param as.data.frame
 #'
 #' @return
 #' @export
 #'
 #' @examples
-GetSegments <- function(globalCompanyId, as.data.frame=TRUE) {
+GetSegments <- function(globalCompanyId, id=NULL, as.data.frame=TRUE) {
 
   endpoint <- sprintf("https://analytics.adobe.io/api/%s",
                       globalCompanyId)
 
-  r <- adobe_get(endpoint, "/segments", AdobeRInternals$auth, globalCompanyId)
+  resource <- "/segments"
+
+  if(!is.null(id)){
+    resource <- paste(resource, "/", id, sep="")
+  }
+
+  r <- adobe_get(endpoint, resource, AdobeRInternals$auth, globalCompanyId)
 
   #Set S3 method for easier parsing later
-  class(r) <- "Segments"
+  if(is.null(id)){
+    class(r) <- "Segments"
+  } else {
+    class(r) <- "Segment"
+  }
 
   #Return a data.frame or just an S3 object
   if(as.data.frame){
@@ -35,6 +46,28 @@ as.data.frame.Segments <- function(x) {
 
 #' @export
 #' @keywords internal
-CompanyAccess <- function(x) {
+Segments <- function(x) {
   UseMethod("Segments", x)
+}
+
+#' @export
+#' @keywords internal
+as.data.frame.Segment <- function(x) {
+
+  df <- as.data.frame(list(
+    id = x$response$id,
+    name = x$response$name,
+    description = x$response$description,
+    rsid = x$response$rsid,
+    onwer.id = x$response$owner$id
+  ))
+
+  return(df)
+
+}
+
+#' @export
+#' @keywords internal
+Segment <- function(x) {
+  UseMethod("Segment", x)
 }
