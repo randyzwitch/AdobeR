f3fea1756eb7d6703e66b9a54a77285fb3ee2750
randyzwitch <rzwitch@gmail.com>
2018-12-15 20:29:27 -0500

    Add id kw to GetUsers

--- R/GetUsers.R
+++ R/GetUsers.R
@@ -7,15 +7,25 @@
 #' @export
 #'
 #' @examples
-GetUsers <- function(globalCompanyId, as.data.frame=TRUE) {
+GetUsers <- function(globalCompanyId, id=NULL, as.data.frame=TRUE) {
 
   endpoint <- sprintf("https://analytics.adobe.io/api/%s",
                       globalCompanyId)
 
-  r <- adobe_get(endpoint, "/users", AdobeRInternals$auth, globalCompanyId)
+  resource <- "/users"
+
+  if(!is.null(id)){
+    resource <- paste(resource, "/", id, sep="")
+  }
+
+  r <- adobe_get(endpoint, resource, AdobeRInternals$auth, globalCompanyId)
 
   #Set S3 method for easier parsing later
-  class(r) <- "Users"
+  if(is.null(id)){
+    class(r) <- "Users"
+  } else {
+    class(r) <- "User"
+  }
 
   #Return a data.frame or just an S3 object
   if(as.data.frame){
@@ -35,6 +45,41 @@ as.data.frame.Users <- function(x) {
 
 #' @export
 #' @keywords internal
-CompanyAccess <- function(x) {
+Users <- function(x) {
   UseMethod("Users", x)
+}
+
+#' @export
+#' @keywords internal
+as.data.frame.User <- function(x) {
+
+  filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
+
+  # df <- as.data.frame(list(
+  #   companyid = filled$companyid,
+  #   loginId = filled$loginId,
+  #   login = filled$login,
+  #   admin = filled$admin,
+  #   createDate = filled$createDate,
+  #   disabled = filled$disabled,
+  #   email = filled$email,
+  #   firstName = filled$firstName,
+  #   fullName = filled$fullName,
+  #   imsUserId = filled$imsUserId,
+  #   lastName = filled$lastName,
+  #   lastLogin = filled$lastLogin,
+  #   lastAccess = filled$lastAccess,
+  #   phoneNumber = filled$phoneNumber,
+  #   tempLoginEnd = filled$tempLoginEnd,
+  #   title = filled$title
+  # ))
+
+  return(as.data.frame(filled))
+
+}
+
+#' @export
+#' @keywords internal
+User <- function(x) {
+  UseMethod("User", x)
 }
