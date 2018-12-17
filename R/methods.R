e3295283acc1732d3ecb6861f86e9aed09cfc96a
randyzwitch <rzwitch@gmail.com>
2018-12-17 09:06:07 -0500

    Convert NULL to NA defensively
    Closes #6

--- R/methods.R
+++ R/methods.R
@@ -1,42 +1,12 @@
+##### as.data.frame methods for S3 classes that already return df
+
 #' @export
 #' @keywords internal
 as.data.frame.CalculatedMetrics <- function(x) x$response$content
 
 #' @export
 #' @keywords internal
-as.data.frame.CalculatedMetric <- function(x) {
-
-  if(is.null(x$response$description)){
-    x$response$description <- NA
-  }
-
-  df <- as.data.frame(list(id = x$response$id,
-                           name = x$response$name,
-                           description = x$response$description,
-                           rsid = x$response$rsid,
-                           polarity = x$response$polarity,
-                           precision = x$response$precision,
-                           type = x$response$type,
-                           owner.id = x$response$owner$id)
-  )
-
-  return(df)
-
-}
-
-#' @export
-#' @keywords internal
-as.data.frame.Dimensions <- function(x) return(x$response)
-
-#' @export
-#' @keywords internal
-as.data.frame.Dimension <- function(x) {
-
-  filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
-
-  return(as.data.frame(filled))
-
-}
+as.data.frame.Dimensions <- function(x) x$response
 
 #' @export
 #' @keywords internal
@@ -44,49 +14,11 @@ as.data.frame.ReportSuites <- function(x) x$response$content
 
 #' @export
 #' @keywords internal
-as.data.frame.ReportSuite <- function(x) {
-
-  df <- as.data.frame(list(collectionItemType = x$response$collectionItemType,
-                           rsid = x$response$rsid)
-  )
-
-  return(df)
-
-}
-
-#' @export
-#' @keywords internal
 as.data.frame.Segments <- function(x) x$response$content
 
 #' @export
 #' @keywords internal
-as.data.frame.Segment <- function(x) {
-
-  df <- as.data.frame(list(
-    id = x$response$id,
-    name = x$response$name,
-    description = x$response$description,
-    rsid = x$response$rsid,
-    onwer.id = x$response$owner$id
-  ))
-
-  return(df)
-
-}
-
-#' @export
-#' @keywords internal
-as.data.frame.Tags <- function(x) return(x$response$content)
-
-#' @export
-#' @keywords internal
-as.data.frame.Tag <- function(x) {
-
-  df <- as.data.frame(list(id = x$response$id,
-                           name = x$response$name))
-  return(df)
-
-}
+as.data.frame.Tags <- function(x) x$response$content
 
 #' @export
 #' @keywords internal
@@ -94,36 +26,139 @@ as.data.frame.Users <- function(x) x$response$content
 
 #' @export
 #' @keywords internal
-as.data.frame.User <- function(x) {
-
-  filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
-
-  return(as.data.frame(filled))
-
-}
-
-#' @export
-#' @keywords internal
 as.data.frame.Metrics <- function(x) x$response
-
-#' @export
-#' @keywords internal
-as.data.frame.Metric <- function(x) {
-
-  filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
-
-  return(as.data.frame(filled))
-
-}
 
 #' @export
 #' @keywords internal
 as.data.frame.DateRanges <- function(x) x$response$content
 
+
+##### as.data.frame methods for S3 classes that need custom df logic
+
+#' @export
+#' @keywords internal
+as.data.frame.CalculatedMetric <- function(x) {
+
+  #Defensively change any NULL values to NA
+  #This is an issue with some methods that silently drop NULL in lists
+  filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
+
+  #Build data.frame, set column names so that no surprises occur
+  df <- as.data.frame(filled)
+  names(df) <- c("id", "name", "description", "rsid", "polarity",
+                 "precision", "type", "owner.id")
+
+  return(df)
+
+}
+
+#' @export
+#' @keywords internal
+as.data.frame.Dimension <- function(x) {
+
+  #Defensively change any NULL values to NA
+  #This is an issue with some methods that silently drop NULL in lists
+  filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
+
+  #Build data.frame, set column names so that no surprises occur
+  df <- as.data.frame(filled)
+  names(df) <- c("id", "title", "name", "type", "category", "support", "pathable",
+                 "extraTitleInfo", "segmentable", "reportable", "supportsDataGovernance")
+
+  return(df)
+
+}
+
+#' @export
+#' @keywords internal
+as.data.frame.ReportSuite <- function(x) {
+
+  #Defensively change any NULL values to NA
+  #This is an issue with some methods that silently drop NULL in lists
+  filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
+
+  #Build data.frame, set column names so that no surprises occur
+  df <- as.data.frame(filled)
+  names(df) <- c("collectionItemType", "rsid")
+
+  return(df)
+
+}
+
+#' @export
+#' @keywords internal
+as.data.frame.Segment <- function(x) {
+
+  #Defensively change any NULL values to NA
+  #This is an issue with some methods that silently drop NULL in lists
+  filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
+
+  #Build data.frame, set column names so that no surprises occur
+  df <- as.data.frame(filled)
+  names(df) <- c("id", "name", "description", "rsid", "owner.id")
+
+  return(df)
+
+}
+
+#' @export
+#' @keywords internal
+as.data.frame.Tag <- function(x) {
+
+  #Defensively change any NULL values to NA
+  #This is an issue with some methods that silently drop NULL in lists
+  filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
+
+  #Build data.frame, set column names so that no surprises occur
+  df <- as.data.frame(filled)
+  names(df) <- c("id", "name")
+
+  return(df)
+
+}
+
+#' @export
+#' @keywords internal
+as.data.frame.User <- function(x) {
+
+  #Defensively change any NULL values to NA
+  #This is an issue with some methods that silently drop NULL in lists
+  filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
+
+  #Build data.frame, set column names so that no surprises occur
+  df <- as.data.frame(filled)
+  names(df) <- c("companyid", "loginId", "login", "createDate", "disabled", "email",
+                 "firstName", "fullName", "imsUserId", "lastName", "lastLogin",
+                 "lastAccess", "phoneNumber", "tempLoginEnd", "title")
+
+  return(df)
+
+}
+
+#' @export
+#' @keywords internal
+as.data.frame.Metric <- function(x) {
+
+  #Defensively change any NULL values to NA
+  #This is an issue with some methods that silently drop NULL in lists
+  filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
+
+  #Build data.frame, set column names so that no surprises occur
+  df <- as.data.frame(filled)
+  names(df) <- c("id", "title", "name", "type", "category", "support", "allocation",
+                 "precision", "calculated", "segmentable", "supportsDataGovernance",
+                 "polarity")
+
+  return(df)
+
+}
+
 #' @export
 #' @keywords internal
 as.data.frame.DateRange <- function(x) {
 
+  #Defensively change any NULL values to NA
+  #This is an issue with some methods that silently drop NULL in lists
   filled <- lapply(x$response, function(x) ifelse(is.null(x), "NA", x))
   df <- as.data.frame(filled)
 
